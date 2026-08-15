d9ee8cf21e084ef7f0e437903e810e229ee7188e
juandavidzuniga1999-byte <juan.david.zuniga.1999@gmail.com>
2026-08-15 16:09:23 -0600

    ajustes para celular

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const burger = document.getElementById('burgerBtn');
-  const navLinks = document.getElementById('navLinks');
-  burger.addEventListener('click', () => {
+const navLinks = document.getElementById('navLinks');
+
+burger.addEventListener('click', () => {
+
     const isOpen = navLinks.classList.toggle('open');
+
     burger.setAttribute('aria-expanded', isOpen);
-  });
-  navLinks.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', () => { navLinks.classList.remove('open'); burger.setAttribute('aria-expanded', 'false'); });
-  });
-  const form = document.getElementById('contactForm');
-  const success = document.getElementById('formSuccess');
-  form.addEventListener('submit', (e) => { e.preventDefault(); success.style.display = 'block'; form.reset(); });
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        navLinks.classList.remove('open');
+
+        burger.setAttribute('aria-expanded', 'false');
+    });
+
+});
+
+// funcion para enfocar foto en galeria
+function abrirLightbox(img) {
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.src = img.src;
+  lightbox.classList.add('activo');
+}
+
+function cerrarLightbox() {
+  document.getElementById('lightbox').classList.remove('activo');
+}
+
+// Opcional: cerrar si le dan clic fuera de la imagen (en el fondo oscuro)
+document.getElementById('lightbox').addEventListener('click', (e) => {
+  if (e.target.id === 'lightbox') {
+    cerrarLightbox();
+  }
+});
